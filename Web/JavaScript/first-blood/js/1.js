15a4fbad1b4e4d6f1a20eabc189e06e436a66a36
Weidows <utsuko27@qq.com>
2020-12-22 22:36:10 +0800

    ✔ Start JavaScript.

---
+++ Web/JavaScript/first-blood/js/1.js
@@ -0,0 +1,11 @@
+/*
+ * @?: *********************************************************************
+ * @Author: Weidows
+ * @Date: 2020-12-20 22:47:20
+ * @LastEditors: Weidows
+ * @LastEditTime: 2020-12-20 22:48:39
+ * @FilePath: \Weidows\Web\JavaScript\first-blood\js\1.js
+ * @Description:
+ * @!: *********************************************************************
+ */
+alert("HelloWorld-2");
