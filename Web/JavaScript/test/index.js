2ad98290ed33575a8138fb540e9e1bef886406f4
Weidows <utsuko27@qq.com>
2021-01-28 18:05:40 +0800

    🧨Sync.

--- Web/JavaScript/test/index.js
+++ Web/JavaScript/test/index.js
@@ -3,7 +3,7 @@
  * @Author: Weidows
  * @Date: 2021-01-07 16:58:40
  * @LastEditors: Weidows
- * @LastEditTime: 2021-01-22 19:25:47
+ * @LastEditTime: 2021-01-26 02:36:13
  * @FilePath: \Weidows\Web\JavaScript\test\index.js
  * @Description:
  * @!: *********************************************************************
